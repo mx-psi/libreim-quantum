c25b3d9dcff9e37b1adb9b31cda31670b953fc22
Pablo <pbaeyens31@gmail.com>
2019-04-27 00:13:56 +0200

    Bell pair

--- diagrams/Main.hs
+++ diagrams/Main.hs
@@ -4,9 +4,20 @@ import Data.Foldable
 
 import Quipper
 import Quipper.Printing
+import QuipperLib.Simulation
+
+import System.Directory
 
 import Classical
 import Print
+
+bellPair :: Circ (Qubit, Qubit)
+bellPair = do
+  (x, y, z) <- qinit (True, False, False)
+  hadamard y
+  toffoli (x, y, z)
+  qterm True x
+  pure (y, z)
 
 img :: FilePath
 img = "img/"
@@ -21,8 +32,12 @@ examples = map
   , ("not.pdf"    , print_generic PDF notCirc qubit)
   , ("and.pdf"    , print_generic PDF andCirc (qubit, qubit))
   , ("wire.pdf"   , print_generic PDF wire qubit)
-  , ("xnor.pdf"   , print_generic PDF xnor (qubit,qubit))
+  , ("xnor.pdf"   , print_generic PDF xnor (qubit, qubit))
+  , ("bell.pdf"   , print_generic PDF bellPair)
   ]
 
 main :: IO ()
-main = for_ examples (uncurry captureTo)
+main = do
+  createDirectoryIfMissing False         img
+  for_                     examples      (uncurry captureTo)
+  run_generic_io           (0 :: Double) bellPair >>= print
